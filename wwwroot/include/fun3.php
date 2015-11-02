8015220c62292fc3f84c7ea0ab01c587d0783ffa
lynn <58339446@qq.com>
2015-11-02 16:31:08 +0800

    解决fun3 BUG

--- wwwroot/include/fun3.php
+++ wwwroot/include/fun3.php
@@ -4,6 +4,7 @@ function fun3(){
 	$after_master = 4;
 	$after_master = "i am include_fun3 update line";
 	$after_master = "i am include_fun3 update line2";
+	$after_master = "i am include_fun3 fun3 bug ok!";
 	return $after_maste;
 }
 ?>
