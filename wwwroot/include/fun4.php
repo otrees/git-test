5fef4ac62378c95cff75534716d5594c1c6016c9
lynn <58339446@qq.com>
2015-11-02 16:39:51 +0800

    解决fun4 BUG

--- wwwroot/include/fun4.php
+++ wwwroot/include/fun4.php
@@ -2,6 +2,7 @@
 function fun4(){
 	$after_master = 1;
 	$after_master = "i am include_fun4 update line";
+	$after_master = "i am include_fun4 bug no！";
 	$after_master = "i am include_fun4 bug ok！";
 	return $after_maste;
 }
