56d5f7d315e4f2e662f84fab5b0e7f846d267400
lynn <58339446@qq.com>
2015-11-02 16:45:01 +0800

    fun4 new bug

--- wwwroot/include/fun4.php
+++ wwwroot/include/fun4.php
@@ -4,6 +4,7 @@ function fun4(){
 	$after_master = "i am include_fun4 update line";
 	$after_master = "i am include_fun4 bug no！";
 	$after_master = "i am include_fun4 bug ok！";
+	$after_master = "i am include_fun4 bug sencond！";
 	return $after_maste;
 }
 ?>
